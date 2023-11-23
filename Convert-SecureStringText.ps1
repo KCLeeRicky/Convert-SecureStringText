4ad54d8fabb0007abbf1a83617301bdfecaeb5f4
Ricky Lee <ricky.lee@disney.com>
2023-11-24 10:45:48 +1100

    Just changed tab alignment. Nothing else

--- Convert-SecureStringText.ps1
+++ Convert-SecureStringText.ps1
@@ -1,26 +1,26 @@
 function Convert-SecureStringText {
-    <#
-        Objective: Convert between plaintext and SecureString text
-    #>    
-        [cmdletbinding()]
-        param (
-            [Parameter(Mandatory=$true)]
-            [ValidateSet("to","from")]
-            [string]$action,
-            [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-            [string]$MyInput
-        )
-    
-        Try {
-            If ($action -eq "to") {
-                $result = $MyInput.Trim() | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString -ErrorAction SilentlyContinue
-            }
-            else {
-                $result = [Net.NetworkCredential]::new('', (($MyInput.Trim()) |ConvertTo-SecureString)).password
-            }
-            return $result
+<#
+    Objective: Convert between plaintext and SecureString text
+#>    
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("to","from")]
+        [string]$action,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string]$MyInput
+    )
+
+    Try {
+        If ($action -eq "to") {
+            $result = $MyInput.Trim() | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString -ErrorAction SilentlyContinue
         }
-        catch {
-                Write-Warning "Unable to convert.....`n`n$($_.Exception)"
+        else {
+            $result = [Net.NetworkCredential]::new('', (($MyInput.Trim()) |ConvertTo-SecureString)).password
         }
+        return $result
     }
+    catch {
+            Write-Warning "Unable to convert.....`n`n$($_.Exception)"
+    }
+}
